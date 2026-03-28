9a27a792c89caddbf7ff32efd137e84b35668e2f
prem kumar <151729765+prem1kr@users.noreply.github.com>
2026-03-28 06:35:24 +0530

    Update auth.js

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -3,7 +3,6 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "../config/jwt.js";
 dotenv.config();
 
 const SECRET_KEY = process.env.SECRET_KEY;
